fc0ad15cacf4eef468089037d9603fd10b95fe57
Jimmy Glasscock <jamesglasscock99@gmail.com>
2020-12-22 18:06:26 -0700

    Initial Commit

---
+++ pokedex2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pokedex2
+//
+//  Created by Jimmy Glasscock on 12/22/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
